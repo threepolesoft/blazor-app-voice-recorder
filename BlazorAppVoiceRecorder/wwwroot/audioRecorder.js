e9da218db1cb502b09a363b84ac7a9239d3ace0e
threepolesoft <threepolesoft@gmail.com>
2024-06-08 10:17:18 +0600

    Add project files.

---
+++ BlazorAppVoiceRecorder/wwwroot/audioRecorder.js
@@ -0,0 +1,29 @@
+﻿let mediaRecorder;
+let audioChunks = [];
+
+async function startRecording() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error("Browser does not support audio recording.");
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = event => {
+        audioChunks.push(event.data);
+    };
+
+    mediaRecorder.start();
+}
+
+function stopRecording() {
+    return new Promise((resolve, reject) => {
+        mediaRecorder.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+            audioChunks = [];
+            resolve(URL.createObjectURL(audioBlob));
+        };
+
+        mediaRecorder.stop();
+    });
+}
